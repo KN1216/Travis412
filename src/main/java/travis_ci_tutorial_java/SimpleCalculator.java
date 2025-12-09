74e2a27e14eadf7d858f98b46e221f77f7c7aaf5
KN1216 <160638286+KN1216@users.noreply.github.com>
2025-12-09 04:21:14 +0000

    test and individual assignment

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,24 @@
 package travis_ci_tutorial_java;
 
-public class SimpleCalculator {
-	public int add(int a, int b) {
-		return a + b;
+import java.util.*;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SimpleCalculatorTest {
+	@Test
+	public void testAdd() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(1, 1),2);
+	}
+	
+	public void testMult() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3),15);
+	}
+	
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3),2);
 	}
 }
